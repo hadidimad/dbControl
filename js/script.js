aec038dda24f80cb02c4e5104b1c65ea6ca2d487
hadidimad <hadiidmad138@gmail.com>
2017-07-20 09:36:24 +0430

    sprate folders

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function w3_open() {
-  document.getElementsByClassName("w3-sidenav")[0].style.display = "block";
-}
-
-function w3_close() {
-  document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
-}
-
