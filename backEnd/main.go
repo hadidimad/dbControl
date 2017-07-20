63cebd9b7faab29e24bdb0aaca701246d5fd9696
hadidimad <hadiidmad138@gmail.com>
2017-07-20 20:46:17 +0430

    connected page to server" "

--- backEnd/main.go
+++ backEnd/main.go
@@ -1,20 +1,128 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"fmt"
 
+	"database/sql"
+
+	"encoding/json"
+
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
+
+type user struct {
+	ID       int
+	Name     string
+	Password string
+	Age      int
+	Work     string
+}
+type tool struct {
+	ID   int
+	Name string
+	Work string
+}
+type table struct {
+	Name string
+	Nums int
+}
 
 //GetTables :write database tables to client in json
 func GetTables(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello world")
+	db, err := sql.Open("postgres", "user=postgres host=localhost port=5432 dbname=dbcontrol ")
+	if err != nil {
+		log.Fatal(err)
+		fmt.Fprintf(w, "error")
+		return
+	}
+	rows, err := db.Query("SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema = 'public' ORDER BY table_name")
+	var tables []*table
+	if err != nil {
+		log.Fatal(err)
+		fmt.Fprintf(w, "error")
+		return
+	}
+	for rows.Next() {
+		var t table
+		rows.Scan(&t.Name)
+		tables = append(tables, &t)
+	}
+	for _, i := range tables {
+		rows, err := db.Query("SELECT COUNT(*) FROM " + i.Name + ";")
+		if err != nil {
+			fmt.Println(err)
+			fmt.Fprintf(w, "error")
+			return
+		}
+		for rows.Next() {
+			var num int
+			rows.Scan(&num)
+			i.Nums = num
+		}
+	}
+	JSONval, err := json.Marshal(tables)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, "error")
+		return
+	}
+	fmt.Fprint(w, string(JSONval))
 }
 
+//GetTableContextUsers : write rquseted tables records
+func GetTableContextUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", "user=postgres host=localhost port=5432 dbname=dbcontrol ")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, "error")
+		return
+	}
+	rows, err := db.Query("SELECT * FROM  users;")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, "error")
+		return
+	}
+	var users []user
+	for rows.Next() {
+		var Tuser user
+		rows.Scan(&Tuser.ID, &Tuser.Name, &Tuser.Password, &Tuser.Age, &Tuser.Work)
+		users = append(users, Tuser)
+	}
+	JSONval, err := json.Marshal(users)
+	fmt.Fprint(w, string(JSONval))
+}
+func GetTableContextTools(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", "user=postgres host=localhost port=5432 dbname=dbcontrol ")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, "error")
+		return
+	}
+	rows, err := db.Query("SELECT * FROM  tools;")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, "error")
+		return
+	}
+	var tools []tool
+	for rows.Next() {
+		var Ttool tool
+		rows.Scan(&Ttool.ID, &Ttool.Name, &Ttool.Work)
+		tools = append(tools, Ttool)
+	}
+	JSONval, err := json.Marshal(tools)
+	fmt.Fprint(w, string(JSONval))
+}
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/tables", GetTables)
+
+	router.Methods("GET").Path("/tables").HandlerFunc(GetTables)
+	router.Methods("GET").Path("/tables/users").HandlerFunc(GetTableContextUsers)
+	router.Methods("GET").Path("/tables/tools").HandlerFunc(GetTableContextTools)
 	http.ListenAndServe(":8080", router)
 }
